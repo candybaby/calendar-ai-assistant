2df2cf65773120752957ba6dd818904a524f535b
tang <tang@fuba.tech>
2024-11-14 21:09:09 +0800

    feat: lineBot get UserID

--- app.js
+++ app.js
@@ -12,9 +12,33 @@ const client = new line.Client(config);
 
 const app = express();
 
+
+app.post('/callback', line.middleware(config), (req, res) => {
+    Promise
+        .all(req.body.events.map(handleEvent))
+        .then((result) => res.json(result))
+        .catch((err) => {
+            console.error(err);
+            res.status(500).end();
+        });
+});
+
 app.get('/', (_req, res) => {
     res.send('Hello World!')
 })
+
+function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+        // ignore non-text-message event
+        return Promise.resolve(null);
+    }
+
+    // create a echoing text message
+    const echo = { type: 'text', text: 'UserId: ' + event.source.userId };
+
+    // use reply API
+    return client.replyMessage(event.replyToken, echo);
+}
 
 app.listen(3000, () => {
     console.log(`Server running on port 3000`);
